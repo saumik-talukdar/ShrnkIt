388040be435ed6c5e44be00924561f158712bcdd
saumik <saumiktalukdar956@gmail.com>
2026-07-03 10:36:25 +0600

    chore: initialize project structure

---
+++ src/main/java/bd/pro/saumik/shrnkit/ShrnkItApplication.java
@@ -0,0 +1,13 @@
+package bd.pro.saumik.shrnkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrnkItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrnkItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bd/pro/saumik/shrnkit/ShrnkItApplicationTests.java
@@ -0,0 +1,13 @@
+package bd.pro.saumik.shrnkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrnkItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
